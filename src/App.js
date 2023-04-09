c7074aaf6c9c3ac14e2ecb287d535f9c7912debd
Shirsho Dipto <shirshodipto@gmail.com>
2023-04-09 11:12:28 +0600

    Set client side project and installations

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Homepage from "./pages/hompages/Homepage";
+import PersonIcon from "@mui/icons-material/Person";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      <PersonIcon />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/hompages/Homepage.jsx
@@ -0,0 +1,5 @@
+import "./homepage.css";
+
+export default function Homepage() {
+  return <div>this the homepage</div>;
+}
