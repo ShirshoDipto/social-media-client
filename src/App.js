c47c990896c514163b2a3172dbdaad7c1142b734
Shirsho Dipto <shirshodipto@gmail.com>
2023-04-09 21:18:09 +0600

    Complete sidebar designing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Homepage from "./pages/hompages/Homepage";
-import PersonIcon from "@mui/icons-material/Person";
 
 function App() {
   return (
     <div className="App">
       <Homepage />
-      <PersonIcon />
     </div>
   );
 }
--- src/pages/hompages/Homepage.jsx
+++ src/pages/hompages/Homepage.jsx
@@ -1,5 +1,18 @@
 import "./homepage.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 export default function Homepage() {
-  return <div>this the homepage</div>;
+  return (
+    <div>
+      <Topbar />
+      <div className="homepageContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </div>
+  );
 }
