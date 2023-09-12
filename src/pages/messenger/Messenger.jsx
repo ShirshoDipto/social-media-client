93013be18fd286b48c444772252f28ae49c37f33
ShirshoDipto <shirshodipto@gmail.com>
2023-09-13 04:54:48 +0600

    Fix minor bug

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -143,10 +143,10 @@ export default function Messenger({ user }) {
   }
 
   async function handleSubmit(e, msgContent) {
-    const msg = await sendSocketEvent(msgContent.value);
-
     e.target.reset();
     msgContent.focus();
+
+    const msg = await sendSocketEvent(msgContent.value);
     setNewMsgs([...newMsgs, msg]);
     await updateConvsForNewMsg(msg);
   }
